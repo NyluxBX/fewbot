afed98c5d05a0c67202ab4a16b2d1613f313fe26
NyluxBX <96978935+NyluxBX@users.noreply.github.com>
2022-01-23 19:02:08 +0100

    Update TA8_AQV&h3n%K!LL.js

--- commands/TA8_AQV&h3n%K!LL.js
+++ commands/TA8_AQV&h3n%K!LL.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message, args) => {
 
 	const Astra = new disbut.MessageMenuOption().setLabel('Astra').setEmoji('931628478334398494').setValue('Astra')
 	const Breach = new disbut.MessageMenuOption().setLabel('Breach').setEmoji('931628645066366986').setValue('Breach')
-	const Brimstone = new disbut.MessageMenuOption().setLabel('Brimstone').setEmoji('931627490722607146󠁧󠁢󠁥󠁮󠁧󠁿').setValue('Brimstone')
+	/*const Brimstone = new disbut.MessageMenuOption().setLabel('Brimstone').setEmoji('931627490722607146󠁧󠁢󠁥󠁮󠁧󠁿').setValue('Brimstone')
 	const Chamber = new disbut.MessageMenuOption().setLabel('Chamber').setEmoji('931628045431898163').setValue('Chamber')
 	const Cypher = new disbut.MessageMenuOption().setLabel('Cypher').setEmoji('931628296523886592').setValue('Cypher')
 	const Jett = new disbut.MessageMenuOption().setLabel('Jett').setEmoji('931628468033175562').setValue('Jett')
@@ -26,16 +26,16 @@ module.exports.run = async (client, message, args) => {
 	const Skye = new disbut.MessageMenuOption().setLabel('Skye').setEmoji('931628652448325633').setValue('Skye')
 	const Sova = new disbut.MessageMenuOption().setLabel('Sova').setEmoji('931628166127181886').setValue('Sova')
 	const Viper = new disbut.MessageMenuOption().setLabel('Viper').setEmoji('931627840032628766').setValue('Viper')
-	const Yoru = new disbut.MessageMenuOption().setLabel('Yoru').setEmoji('931628576636301332').setValue('Yoru')
+	const Yoru = new disbut.MessageMenuOption().setLabel('Yoru').setEmoji('931628576636301332').setValue('Yoru')*/
 
-	const agentsList = [Astra, Breach, Brimstone, Chamber, Cypher, Jett, Kayo, Killjoy, Neon, Omen, Phoenix, Raze, Reyna, Sage, Skye, Sova, Viper, Yoru]
+	const agentsList = [Astra, Breach/*, Brimstone, Chamber, Cypher, Jett, Kayo, Killjoy, Neon, Omen, Phoenix, Raze, Reyna, Sage, Skye, Sova, Viper, Yoru*/]
 
 	const agentsSelector = new disbut.MessageMenu()
 	    .setID('agentsSelector')
 	    .setPlaceholder('Agents VALORANT').setMaxValues(3).setMinValues(1)
 	    .addOption(Astra)
 	    .addOption(Breach)
-	    .addOption(Brimstone)
+	    /*.addOption(Brimstone)
 	    .addOption(Chamber)
 	    .addOption(Cypher)
 	    .addOption(Jett)
@@ -50,7 +50,7 @@ module.exports.run = async (client, message, args) => {
 	    .addOption(Skye)
 	    .addOption(Sova)
 	    .addOption(Viper)
-	    .addOption(Yoru)
+	    .addOption(Yoru)*/
 
 
 	message.channel.send(agentsEmbed, agentsSelector);
