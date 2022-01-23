4b1ecb69dbfc4e33e7c6187e2ebe0a357007d544
NyluxBX <96978935+NyluxBX@users.noreply.github.com>
2022-01-23 19:28:05 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,4 +7,5 @@ module.exports = async (client, member) => {
     member.roles.add('867767672469192754')
     member.roles.add('867777630921031730')
     member.roles.add('867078962752651274')
+    member.roles.add('934147261775106088')
 }
