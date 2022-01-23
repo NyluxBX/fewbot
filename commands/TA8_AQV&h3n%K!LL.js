f4aa5e85f242b101eae914af1ac53d856e27ba15
NyluxBX <96978935+NyluxBX@users.noreply.github.com>
2022-01-23 19:06:09 +0100

    Update TA8_AQV&h3n%K!LL.js

--- commands/TA8_AQV&h3n%K!LL.js
+++ commands/TA8_AQV&h3n%K!LL.js
@@ -11,11 +11,11 @@ module.exports.run = async (client, message, args) => {
 
 	const Astra = new disbut.MessageMenuOption().setLabel('Astra').setEmoji('931628478334398494').setValue('Astra')
 	const Breach = new disbut.MessageMenuOption().setLabel('Breach').setEmoji('931628645066366986').setValue('Breach')
-	/*const Brimstone = new disbut.MessageMenuOption().setLabel('Brimstone').setEmoji('931627490722607146󠁧󠁢󠁥󠁮󠁧󠁿').setValue('Brimstone')
+	const Brimstone = new disbut.MessageMenuOption().setLabel('Brimstone').setEmoji('931627490722607146󠁧󠁢󠁥󠁮󠁧󠁿').setValue('Brimstone')
 	const Chamber = new disbut.MessageMenuOption().setLabel('Chamber').setEmoji('931628045431898163').setValue('Chamber')
 	const Cypher = new disbut.MessageMenuOption().setLabel('Cypher').setEmoji('931628296523886592').setValue('Cypher')
 	const Jett = new disbut.MessageMenuOption().setLabel('Jett').setEmoji('931628468033175562').setValue('Jett')
-	const Kayo = new disbut.MessageMenuOption().setLabel('KAY/O').setEmoji('931628298612641842').setValue('Kayo')
+	/*const Kayo = new disbut.MessageMenuOption().setLabel('KAY/O').setEmoji('931628298612641842').setValue('Kayo')
 	const Killjoy = new disbut.MessageMenuOption().setLabel('Killjoy').setEmoji('931628031288688641').setValue('Killjoy')
 	const Neon = new disbut.MessageMenuOption().setLabel('Neon').setEmoji('931628213338263582').setValue('Neon')
 	const Omen = new disbut.MessageMenuOption().setLabel('Omen').setEmoji('931627923197272134').setValue('Omen')
@@ -28,18 +28,18 @@ module.exports.run = async (client, message, args) => {
 	const Viper = new disbut.MessageMenuOption().setLabel('Viper').setEmoji('931627840032628766').setValue('Viper')
 	const Yoru = new disbut.MessageMenuOption().setLabel('Yoru').setEmoji('931628576636301332').setValue('Yoru')*/
 
-	const agentsList = [Astra, Breach/*, Brimstone, Chamber, Cypher, Jett, Kayo, Killjoy, Neon, Omen, Phoenix, Raze, Reyna, Sage, Skye, Sova, Viper, Yoru*/]
+	const agentsList = [Astra, Breach, Brimstone, Chamber, Cypher, Jett, /*Kayo, Killjoy, Neon, Omen, Phoenix, Raze, Reyna, Sage, Skye, Sova, Viper, Yoru*/]
 
 	const agentsSelector = new disbut.MessageMenu()
 	    .setID('agentsSelector')
 	    .setPlaceholder('Agents VALORANT').setMaxValues(3).setMinValues(1)
 	    .addOption(Astra)
 	    .addOption(Breach)
-	    /*.addOption(Brimstone)
+	    .addOption(Brimstone)
 	    .addOption(Chamber)
 	    .addOption(Cypher)
 	    .addOption(Jett)
-	    .addOption(Kayo)
+	    /*.addOption(Kayo)
 	    .addOption(Killjoy)
 	    .addOption(Neon)
 	    .addOption(Omen)
